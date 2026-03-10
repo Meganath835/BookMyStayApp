71bb4a3cc67f24ddb3096f0d56a444afe3ba8941
Meganath835 <gmmeganath@gmail.com>
2026-03-10 09:15:24 +0530

    UC1:Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,12 @@
+public class BookMyStayApp
+    /*
+    @author Developer
+    @version 1.0
+    */
+{
+    public static void main(String[] args){
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System Initialized Successfully.");
+
+    }
+}
